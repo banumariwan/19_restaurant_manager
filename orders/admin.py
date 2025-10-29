8e0d6090f7101c3deff9a1692d860c858d346846
Banu Mariwan <mariwanburhan5@gmail.com>
2025-10-30 02:42:55 +0300

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from .models import MenuItem, Table, Order
 
-# Register MenuItem
+
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'available')
     list_filter = ('category', 'available')
     search_fields = ('name', 'category')
 
-# Register Table
+
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ('number', 'seats', 'status')
     list_filter = ('status',)
     search_fields = ('number',)
 
-# Register Order
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'table', 'created_by', 'total_price', 'status', 'created_at')
